7bf2c944d8a87d02b19a0690743829fb3e5c3816
Daniel Hopson <hdhopson88@yahoo.com>
2021-01-10 01:32:18 -0800

    Adds back button to navigate to home page on click

--- src/app/list-details-page/list-details-page.component.ts
+++ src/app/list-details-page/list-details-page.component.ts
@@ -1,6 +1,7 @@
-import { ListDetailsService } from './../services/list-details/list-details.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
+import { ListDetailsService } from './../services/list-details/list-details.service';
 import { ListUI } from '../models/list-details.model';
 import { Item } from '../models/item-details.model';
 
@@ -14,7 +15,7 @@ export class ListDetailsPageComponent implements OnInit {
   itemedList: ListUI;
   items: Item[];
 
-  constructor(private service: ListDetailsService) {
+  constructor(private service: ListDetailsService, private router: Router) {
     this.itemedList = {
       id: "",
       title: "",
@@ -27,6 +28,7 @@ export class ListDetailsPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.router.events.subscribe();
     this.getItemedList("ef6a7925-80a6-44bc-9e03-89bc7ca3586e");
   }
 
@@ -35,5 +37,9 @@ export class ListDetailsPageComponent implements OnInit {
       this.itemedList = res;
       this.items = this.itemedList.items;
     });
+  }
+
+  toHomePage() {
+    this.router.navigate([""])
   }
 }
